3be54ff6f71f5cc078d321f418bf05a0207f90ff
JayNguyen168 <tragiang1682003@gmail.com>
2023-12-01 19:23:13 +0700

    prj_init

---
+++ src/queue.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t * q) {
+        if (q == NULL) return 1;
+	return (q->size == 0);
+}
+
+void enqueue(struct queue_t * q, struct pcb_t * proc) {
+        /* TODO: put a new process to queue [q] */
+        if(q->size < MAX_QUEUE_SIZE){
+                q->proc[q->size++] = proc;
+        }
+}     
+
+struct pcb_t * dequeue(struct queue_t * q) {
+        /* TODO: return a pcb whose prioprity is the highest
+         * in the queue [q] and remember to, remove it from q
+         * */
+	if(empty(q)) return NULL;
+        int max = 0;
+        for(int i = 0; i < MAX_QUEUE_SIZE; i++){
+                if(q->proc[i]->priority < q->proc[max]->priority){
+                        max = i;
+                }
+        }
+        struct pcb_t *res = q->proc[max];
+        for (int i = max + 1; i < q->size; i++){
+                q->proc[i-1] = q->proc[i];
+        }
+        q -> size--;
+        return res;
+}
+
